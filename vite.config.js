9996241f36a1502abe692248affe3239ee7bedb3
Emman091 <emmanuelkenneth063@mail.com>
2026-06-25 17:54:38 +0100

    Fix sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+
 import Sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
+
   plugins: [
+
     react(),
+
     Sitemap({
+
       hostname: 'https://react-portfolio-blond-two-25.vercel.app',
+
     }),
+
   ],
+
 })
